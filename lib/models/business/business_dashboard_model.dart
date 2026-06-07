10f97e883cb32504bf68bdc5e160e4bfc87b0363
MehribanShukrieva <merhiban.shukrieva@apollo-gs.com>
2026-06-07 16:47:32 +0300

    feat: Add business reports page and load API statistics

--- lib/models/business/business_dashboard_model.dart
+++ lib/models/business/business_dashboard_model.dart
@@ -1,16 +1,50 @@
 class BusinessDashboardModel {
   final List<double> weeklySales;
   final double co2Saved;
+  final int totalOrders;
+  final int activeBags;
+  final int pickedUpOrders;
+  final double rating;
+  final int soldOutBags;
+  final double totalRevenue;
+  final int mealsSaved;
+  final int unsoldItems;
+  final double co2SavedKg;
+  final double wastePreventedKg;
+  final double sellThroughRate;
 
   BusinessDashboardModel({
     required this.weeklySales,
     required this.co2Saved,
+    required this.totalOrders,
+    required this.activeBags,
+    required this.pickedUpOrders,
+    required this.rating,
+    required this.soldOutBags,
+    required this.totalRevenue,
+    required this.mealsSaved,
+    required this.unsoldItems,
+    required this.co2SavedKg,
+    required this.wastePreventedKg,
+    required this.sellThroughRate,
   });
 
   factory BusinessDashboardModel.fromJson(Map<String, dynamic> json) {
+    final summary = json['summary'] as Map<String, dynamic>? ?? {};
     return BusinessDashboardModel(
       weeklySales: _parseWeeklySales(json['weekly_sales']),
       co2Saved: (json['co2_saved'] as num?)?.toDouble() ?? 0.0,
+      totalOrders: (summary['total_orders'] as num?)?.toInt() ?? 0,
+      activeBags: (summary['active_bags'] as num?)?.toInt() ?? 0,
+      pickedUpOrders: (summary['picked_up_orders'] as num?)?.toInt() ?? 0,
+      rating: (summary['rating'] as num?)?.toDouble() ?? 0.0,
+      soldOutBags: (summary['sold_out_bags'] as num?)?.toInt() ?? 0,
+      totalRevenue: (summary['total_revenue'] as num?)?.toDouble() ?? 0.0,
+      mealsSaved: (summary['meals_saved'] as num?)?.toInt() ?? 0,
+      unsoldItems: (summary['unsold_items'] as num?)?.toInt() ?? 0,
+      co2SavedKg: (summary['co2_saved_kg'] as num?)?.toDouble() ?? 0.0,
+      wastePreventedKg: (summary['waste_prevented_kg'] as num?)?.toDouble() ?? 0.0,
+      sellThroughRate: (summary['sell_through_rate'] as num?)?.toDouble() ?? 0.0,
     );
   }
 
